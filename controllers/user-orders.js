ce2178fb951260300c880547cc8eba908785a447
AyushPathak110 <ayushpathak1102004@gmail.com>
2025-01-16 17:13:21 +0530

    Changes in frontend

--- controllers/user-orders.js
+++ controllers/user-orders.js
@@ -2,7 +2,7 @@ const Booking = require("../models/bookinmodel");
 const delivery = require("../models/delivery");
 
 async function getYourOrders(req, res) {
-  const data = await Booking.find({ "user.email": req.user.email }).populate("rider");
+  const data = await Booking.find({ "user.email": req.user.email }).populate("rider").populate("sname");
   res.render("../views/viewConfirmed.ejs", { data });
 }
  
